495c2ad2e6013c786e34bdba563a00cd3b5ccbaa
Zokyn <guttycasaes@hotmail.com>
2025-04-14 20:35:22 -0300

    refac: ajusta animação de imagem que causava sobreposição na navbar

--- script.js
+++ script.js
@@ -6,17 +6,31 @@ document.addEventListener("DOMContentLoaded", () => {
     // Pega todos os checkboxes que pretendo colocar a função
     const checkboxesElements = document.querySelectorAll('.select-item-check');
 
+    const itemPicturesElements = document.querySelectorAll('.select-item-picture');
+
     // Transforma esse objeto de checkboxes em um array
     const checkboxesList = Array.from(checkboxesElements);
-    console.log(checkboxesList)
+
+    const itemPicturesList = Array.from(itemPicturesElements);
+
     // Visita cada checkbox e adiciona um eventlistener da função
     checkboxesList.forEach(checkbox => {
-        console.log(checkbox)
-        checkbox.addEventListener('change', function (checkbox) {
-            if (this.checked)
+        // Adiciona o event listener para quando o checkbox mudar
+        checkbox.addEventListener('change', function () {
+            if (this.checked) // Se ele estiver "marcado"
+                // O elemento pai (li) deverá ser selecionado
                 this.parentElement.classList.add('selected');
             else 
+                // Caso contrário o elemento pai deverá ser desselecionado
                 this.parentElement.classList.remove('selected');
+        })
+    })
+    // Visita cada image do item e adiciona um eventlistener da função
+    itemPicturesList.forEach(picture => {
+        // Adiciona o evento listener para quando clicarem na image
+        picture.addEventListener('click', function () {
+            // Vai até o element ovô (li) e ativa ou desativa class "selected"
+            this.parentElement.parentElement.classList.toggle('selected');
         })
     })
 });
