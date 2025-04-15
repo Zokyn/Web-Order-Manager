ac3063ce61c43a476df8120e75dbf08237f71932
Zokyn <guttycasaes@hotmail.com>
2025-04-15 15:21:03 -0300

    feat: adicionado protótipo de carousel na pagina index

--- script.js
+++ script.js
@@ -67,4 +67,72 @@ document.addEventListener("DOMContentLoaded", () => {
             itemContainer.classList.remove('unable');
         })
     })
+    /* --- Carousel --- */
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.slide');
+    const dotsContainer = document.querySelector('.dots-container');
+    const slidesContainer = document.querySelector('.slides-container');
+
+    // Para cada Slide encontrado na pagina cria um dot
+    slides.forEach((_, index) => {
+        if(index%3 == 0) {
+            // Dot nada mais é que um span com className dot
+            const dot = document.createElement('span');
+            dot.classList.add('dot');
+
+            // Se ele for o primeiro da lista, o coloca como ativo
+            if (index === 0)
+                dot.classList.add('active');
+
+            // Adiciona eventListener para ao clicar e para o slide
+            dot.addEventListener('click', () => goToSlide(index));
+
+            // Adiciona-o ao container de dots (dotsContainer)
+            dotsContainer.appendChild(dot);
+        }
+
+    })
+
+    const dots = document.querySelectorAll('.dot');
+    console.log(dots)
+    function goToSlide(index) {
+        // Se o index for 
+        if (index >= slides.length/3) // maior que o numero de slides
+            index = 0 // então o index do slide deve reiniciar indo a 0
+        // Se o index for
+        if (index < 0) // menor do que zero 
+            index = slides.length - 1; // então index deve ir para o último slide
+        
+        slidesContainer.style.transform = `translateX(-${index * 100}%)`;
+
+        slides[currentSlide].classList.remove('active');
+        dots[currentSlide].classList.remove('active');
+
+        slides[index].classList.add('active');
+        dots[index].classList.add('active');
+
+        currentSlide = index;
+    }
+
+    document.querySelector('.prev').addEventListener('click', () => {
+        goToSlide(currentSlide - 1);
+    })
+
+    document.querySelector('.next').addEventListener('click', () => {
+        goToSlide(currentSlide + 1);
+    })
+    
+    let autoplay = setInterval(() => {
+        goToSlide(currentSlide + 1);
+    }, 5000);
+
+    document.querySelector('index-carousel').addEventListener('mouseenter', () => {
+        clearInterval(autoplay);
+    });
+
+    document.querySelector('index-carousel').addEventListener('mouseleave', () => {
+        autoplay = setInterval(() => {
+            goToSlide(currentSlide + 1);
+        }, 5000)
+    })
 });
