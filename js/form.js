1b42dc8f98686fc113674f9c38e682ce83cef434
Zokyn <guttycasaes@hotmail.com>
2025-04-25 21:40:35 -0300

    refac(comments): adicionado comentários as cores do body

--- js/form.js
+++ js/form.js
@@ -1,7 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    errorsElements = document.querySelectorAll('.error');
+document.addEventListener('DOMContentLoaded', function() { 
+    const resetButton = document.querySelector('button[type="reset"]');
+    resetButton.disabled = true;
+
+    const form = document.querySelector('form');
+
+    const inputsElements = document.querySelectorAll('[name*="-input"');
+    const errorsElements = document.querySelectorAll('[name*="-error"');
+    console.log(inputsElements)
+    console.log(errorsElements)
 
     errorsElements.forEach((error) => {
         error.classList.add('hidden');
+    })
+
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault;
+    })
+
+    form.addEventListener('input', () => {
+        resetButton.disabled = false;
+    })
+
+    inputsElements.forEach((input, index) => {
+        input.addEventListener('invalid', (e) => {
+            errorsElements[index].classList.remove('hidden')
+        })
     })
 });
